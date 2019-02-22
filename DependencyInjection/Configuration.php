5513c5f8ff66b65eda28152cacba9a5b449661ac
Daniel Leeb <dl@loremipsum.at>
2019-02-22 09:29:58 +0100

    Update Configuration

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -2,6 +2,8 @@
 
 namespace LoremIpsum\PermissionCheckerBundle\DependencyInjection;
 
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\Config\Definition\Builder\NodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -9,15 +11,9 @@ class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder('lorem_ipsum_permission_checker');
-        if (method_exists($treeBuilder, 'getRootNode')) {
-            $rootNode = $treeBuilder->getRootNode();
-        } else {
-            // BC layer for symfony/config 4.1 and older
-            $rootNode = $treeBuilder->root('lorem_ipsum_permission_checker');
-        }
-
-        $rootNode
+        $name        = 'lorem_ipsum_permission_checker';
+        $treeBuilder = new TreeBuilder($name);
+        $this->getRootNode($treeBuilder, $name)
             ->children()
                 ->arrayNode('roles')
                     ->isRequired()
@@ -31,5 +27,19 @@ class Configuration implements ConfigurationInterface
         ;
 
         return $treeBuilder;
+    }
+
+    /**
+     * @param TreeBuilder $treeBuilder
+     * @param string      $name
+     * @return ArrayNodeDefinition|NodeDefinition
+     */
+    private function getRootNode(TreeBuilder $treeBuilder, string $name)
+    {
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            return $treeBuilder->getRootNode();
+        }
+        // BC layer for symfony/config 4.1 and older
+        return $treeBuilder->root($name);
     }
 }
