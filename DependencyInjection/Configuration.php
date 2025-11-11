b03c06f4a4cee1249e9b96e454d402bb5eb457e1
Daniel Leeb <dl@loremipsum.at>
2025-11-11 09:42:57 +0100

    Update dependencies

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -9,7 +9,7 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 class Configuration implements ConfigurationInterface
 {
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $name        = 'lorem_ipsum_permission_checker';
         $treeBuilder = new TreeBuilder($name);
